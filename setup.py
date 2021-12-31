e2f82d93c91453a77a52d063e6e14244ab94a351
Tanel Vakker <tanel@vakker.org>
2021-12-31 19:14:14 +0200

    Update to myupway

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-  name='nibe_downlink',
+  name='myupway_downlink',
   version='1.0',
-  packages=['nibe_downlink'],
+  packages=['myupway_downlink'],
   url='',
   license='LGPL 3',
   author='yozik04',
   author_email='yozik04@gmail.com',
-  description='Fetches metrics from Nibe Uplink'
+  description='Fetches metrics from MyUpway'
 )
